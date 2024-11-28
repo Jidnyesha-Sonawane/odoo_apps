84916bf540ef01e36fcdb7445b8334f69960ec41
Jidnyesha-Sonawane <jidnyeshasonawane2209@gmail.com>
2024-11-28 16:07:27 +0530

    First commit.

---
+++ custom_website_snippets/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Custom Website Snippets",
+    "summary": """ Custom Website Snippets """,
+    "description": """ Custom Website Snippets. """,
+    "version": "1.0",
+    "author": "Jidnyesha Sonawane",
+    "category": "Website",
+    "depends": ["website", "website_sale"],
+    "data": [
+        'views/snippets/s_product.xml'
+    ],
+    "assets": {
+        "web.assets_frontend": [],
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "license": "LGPL-3",
+}
